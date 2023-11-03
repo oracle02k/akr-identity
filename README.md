ddb0cfbb49a32937c5742db3470cf199b02680a0
oracle02k <oracle02k@gmail.com>
2023-11-03 23:24:49 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# akr-identity
+
+akr-identity utilizes psalm's static analysis to provide type Id.
+
+## motivation
+
+- Safely operate each Id that is generated in large quantities of master data, etc.
+- In the simple way, all Ids are of type int, so there is no error even if irrelevant Ids are compared.
+- Therefore, we want to apply a dedicated type to each Id so that it cannot be compared with any Id other than the same type.
+
+## spec
+
+- akr-identity detects the comparison of different types by static analysis and throws an error.
+- akr-identity caches values so you can take advantage of strict comparisons.
---
+++ tests/IdentityTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+use akr\identity\Identity;
+use PHPUnit\Framework\TestCase;
+
+class Base
+{
+};
+class Other
+{
+};
+class Derived extends Base
+{
+};
+
+final class IdentityTest extends TestCase
+{
+    public function testShouldBeIdentityStrictlyComparable(): void
+    {
+        $id1 = Identity::create(Base::class, 0);
+        $id2 = Identity::create(Base::class, 0);
+        $id3 = Identity::create(Base::class, 1);
+
+        $this->assertTrue($id1 === $id2);
+        $this->assertFalse($id1 === $id3);
+    }
+
+    public function testShouldBeFalseWhenComparingIdentitiesOfDifferentTypes(): void
+    {
+        $id1 = Identity::create(Base::class, 0);
+        $id2 = Identity::create(Other::class, 0);
+
+        //$this->assertFalse($id1 === $id2); // If static analysis is enabled, an error occurs because the type is different.
+        $this->assertTrue(true); //dummy assertion.
+    }
+
+    public function testShouldBeFalseWhenComparingBaseClassAndDerivedClass(): void
+    {
+        $id1 = Identity::create(Base::class, 0);
+        $id2 = Identity::create(Derived::class, 0);
+
+        //$this->assertFalse($id1 === $id2); // If static analysis is enabled, an error occurs because the type is different.
+        $this->assertTrue(true); //dummy assertion.
+    }
+}
