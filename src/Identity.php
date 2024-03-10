5a35e7fab71339e2a7360749967ddbf691b959b7
oracle02k <oracle02k@gmail.com>
2024-03-10 15:10:13 +0000

    Fixed to remove unused Identities from the cache.

--- src/Identity.php
+++ src/Identity.php
@@ -5,32 +5,42 @@ declare(strict_types=1);
 namespace akr\identity;
 
 /**
- * @template T of object
  * @psalm-immutable
+ * @template T of object
  */
 class Identity
 {
-    /** @var Identity[] */
+    /** @var array<string, \WeakReference<Identity>> */
     private static array $instances = [];
 
     /**
      * @template TClass of object
      * @param class-string<TClass> $class
-     * @param int                  $value
      * @return self<TClass>
      */
     public static function create(string $class, int $value): self
     {
-        $key = "{$class}_{$value}";
+        $key = self::makeKey($class, $value);
+
         if (array_key_exists($key, self::$instances)) {
-            /** @var self<TClass> */
-            return self::$instances[$key];
+            /** @var Identity<TClass> */
+            return self::$instances[$key]->get();
         }
 
         $identity = new self($class, $value);
-        self::$instances[$key] = $identity;
+        self::$instances[$key] = \WeakReference::create($identity);
 
         return $identity;
+    }
+
+    /**
+     * @psalm-pure
+     * @template TClass of object
+     * @param class-string<TClass> $class
+     */
+    public static function makeKey(string $class, int $value): string
+    {
+        return "{$class}_{$value}";
     }
 
     /**
@@ -38,5 +48,16 @@ class Identity
      */
     private function __construct(readonly string $typeName, readonly int $value)
     {
+    }
+
+    public function __destruct()
+    {
+        /** @psalm-suppress ImpureStaticProperty */
+        unset(self::$instances[$this->key()]);
+    }
+
+    public function key(): string
+    {
+        return self::makeKey($this->typeName, $this->value);
     }
 }
